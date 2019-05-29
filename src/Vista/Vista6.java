903c4655c26b4a2bec961b5e0802c7d7519f208f
Lauraradi <51141370+Lauraradi@users.noreply.github.com>
2019-05-29 13:37:07 -0500

    pp

--- src/Vista/Vista6.java
+++ src/Vista/Vista6.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vista;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- *
- * @author laura
- */
 public class Vista6 extends javax.swing.JFrame implements ActionListener{
 
     /**
